45cc0349701bacd203d164691727f69472b40459
buptzy <49507769+buptzy@users.noreply.github.com>
2019-06-01 14:20:07 +0800

    登陆界面

---
+++ sign_in.py
@@ -0,0 +1,50 @@
+#登陆界面
+from PyQt5 import QtCore, QtGui, QtWidgets, Qt
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+class Sign(QtWidgets.QMainWindow):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(386, 127)
+        MainWindow.setWindowIcon(QIcon('logo.png'))
+        MainWindow.setStyleSheet("background-image:url(Background.jpg)")
+        self.centralWidget = QtWidgets.QWidget(MainWindow)
+        self.centralWidget.setObjectName("centralWidget")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralWidget)
+        self.lineEdit.setGeometry(QtCore.QRect(250, 20, 100, 20))
+        self.lineEdit.setText("")
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralWidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(250, 50, 100, 20))
+        self.lineEdit_2.setText("")
+        self.lineEdit_2.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.label = QtWidgets.QLabel(self.centralWidget)
+        self.label.setGeometry(QtCore.QRect(200, 24, 32, 16))
+        self.label.setTextFormat(QtCore.Qt.AutoText)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralWidget)
+        self.label_2.setGeometry(QtCore.QRect(200, 54, 32, 16))
+        self.label_2.setObjectName("label_2")
+        self.pushButton = QtWidgets.QPushButton(self.centralWidget)
+        self.pushButton.setGeometry(QtCore.QRect(190, 90, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralWidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(290, 90, 75, 23))
+        self.pushButton_2.setObjectName("pushButton_2")
+        MainWindow.setCentralWidget(self.centralWidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "登陆账号"))
+        self.lineEdit.setPlaceholderText(_translate("MainWindow", "请输入帐号"))
+        self.lineEdit_2.setPlaceholderText(_translate("MainWindow", "请输入密码"))
+        self.label.setText(_translate("MainWindow", "帐号"))
+        self.label_2.setText(_translate("MainWindow", "密码"))
+        self.pushButton.setText(_translate("MainWindow", "确定"))
+        self.pushButton_2.setText(_translate("MainWindow", "取消"))
